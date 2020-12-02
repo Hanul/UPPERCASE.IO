86593b68aeeab7154b5f8476178fdcca3513bca2
hanul <hanul@hanul.me>
2020-12-02 22:33:20 +0900

    오래된 내용 제거

--- __PACK/UPPERCASE_IO/NODE.js
+++
@@ -1,1 +0,0 @@
-UPPERCASE_IO.MAIN=METHOD({run:(e,n)=>{let s=require("less");e(SML.Bridge({rootPath:"./UPPERCASE_IO/view"}).requestListener),n({extension:"less",preprocessor:(e,n)=>{s.render(e,(e,s)=>{n({content:s.css,contentType:"text/css",version:CONFIG.version})})}})}});
--- __PACK/UPPERCASE_IO/README.md
+++
@@ -1,2 +0,0 @@
-# uppercase.io
-[http://uppercase.io](http://uppercase.io)의 소스코드
